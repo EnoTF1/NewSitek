4313f19bca6b9d8e41b57b9d2961edd3e7f6dc26
EnoTF1 <155848089+EnoTF1@users.noreply.github.com>
2024-01-27 09:11:14 +0000

    UAHDAHDHAHWd

---
+++ script.js
@@ -0,0 +1,9 @@
+function bebraF(){
+    let img = document.getElementById("map");
+    img.src = "";
+  }
+
+  function bebraA(){
+    let img = document.getElementById("map");
+    img.src = "https://i.imgur.com/mlxFcPn.jpeg";
+  }
